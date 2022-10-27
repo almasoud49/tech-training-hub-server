bad49804fc9b3c736ac2a9a768d38b73d16ef866
Abdullah Al Masoud <almasoud49@yahoo.com>
2022-10-27 11:54:02 +0600

    api added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) =>{
     res.send('Tech Training Hub')
@@ -13,6 +14,28 @@ app.get('/', (req, res) =>{
 
 app.get('/course-categories', (req, res) =>{
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res) =>{
+    const id =req.params.id;
+    if(id === '08'){
+        res.send(courses);
+    }
+    else{
+    const courseCategory = courses.filter(course => course.category_id === id);
+    res.send(courseCategory);  
+    }
+   
+})
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+})
+
+app.get('/course/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, ()=> {
